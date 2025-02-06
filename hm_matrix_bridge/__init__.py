9fb0aea888ea115913c418cbfe7c844ea8475151
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-02-06 23:18:30 +0000

    refactor!:move to solver (#4)

--- hm_matrix_bridge/__init__.py
+++ hm_matrix_bridge/__init__.py
@@ -1,25 +1,18 @@
-from ovos_utils.log import LOG
-from ovos_utils import create_daemon
-from ovos_utils.messagebus import Message
-from time import sleep
 from hm_matrix_bridge.matrixapi import MatrixBot
-from hivemind_bus_client.client import HiveMessageBusClient
-from hivemind_bus_client.message import HiveMessage, HiveMessageType
-from hivemind_bus_client.identity import NodeIdentity
-from ovos_utils.messagebus import FakeBus
+from ovos_hivemind_solver import HiveMindSolver
+from ovos_utils.log import LOG
 
 
-class HiveMindMatrixBridge(HiveMessageBusClient):
-    platform = "HiveMindMatrixBridgeV0.1"
+class HiveMindMatrixBridge:
+    platform = "HiveMindMatrixBridgeV0.2"
 
-    def __init__(self, matrix_host, matrix_token, room_alias, bot_mention=None,
-                 greeting=None, *args, **kwargs):
+    def __init__(self, matrix_host, matrix_token, room_alias, bot_mention=None, greeting=None):
         self.bot = MatrixBot(matrix_host, matrix_token, room_alias, bot_mention, greeting)
         self.bot.on_message = self.handle_matrix_utterance
         LOG.info("== connected to Matrix")
-        super().__init__(*args, **kwargs)
-        self.connect(FakeBus(), site_id="matrix")
-        self.on_mycroft("speak", self.handle_incoming_mycroft)
+        self.solver = HiveMindSolver(config={"site_id": "matrix",
+                                             "useragent": self.platform,
+                                             "autoconnect": True})
         LOG.info("== connected to HiveMind")
 
     def handle_matrix_utterance(self, event):
@@ -27,21 +20,14 @@ class HiveMindMatrixBridge(HiveMessageBusClient):
         LOG.debug(f"{event['sender']}: {utt}")
         if self.bot.bot_mention in utt:
             LOG.debug("bot mentioned")
-            utt = utt.replace(f"@{self.bot.bot_mention}", "")\
-                .replace(f"{self.bot.bot_mention}:", "")\
+            utt = utt.replace(f"@{self.bot.bot_mention}", "") \
+                .replace(f"{self.bot.bot_mention}:", "") \
                 .replace(self.bot.bot_mention, "").strip()
 
             # TODO - lang detection plugin here
             LOG.debug(f"asking hivemind: {utt}")
-            self.emit_mycroft(
-                Message("recognizer_loop:utterance",
-                        {"utterances": [utt]},
-                        {"destination": "skills"})
-            )
+            utterance = self.solver.get_spoken_answer(utt)
+            LOG.info(f"HiveMind: {utterance}")
+            self.bot.room.send_text(utterance or "Error")
         else:
             LOG.debug("bot not mentioned. ignoring")
-
-    def handle_incoming_mycroft(self, message):
-        utterance = message.data["utterance"]
-        LOG.info(f"HiveMind: {utterance}")
-        self.bot.room.send_text(utterance)
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 
 ## Usage
 
+set hivemind credentials in identity file
+
 ```
 Usage: HiveMind-matrix run [OPTIONS]
 
@@ -14,10 +16,6 @@ Options:
   --matrixtoken TEXT  syt_dGhl.....
   --matrixhost TEXT   https://matrix.org
   --room TEXT         #hivemind-bots:matrix.org
-  --key TEXT          HiveMind access key (default read from identity file)
-  --password TEXT     HiveMind password (default read from identity file)
-  --host TEXT         HiveMind host (default read from identity file)
-  --port INTEGER      HiveMind port number (default: 5678)
   --help              Show this message and exit.
 
 ```
