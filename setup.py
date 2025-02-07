826c6ef38009d860b454518a9eea794d6e03afee
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-02-07 13:52:01 +0000

    semver (#5)

--- setup.py
+++ setup.py
@@ -1,16 +1,60 @@
 from setuptools import setup
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+with open(f"{BASEDIR}/README.md", "r") as fh:
+    long_desc = fh.read()
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_hivemind_solver', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if int(alpha):
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
 
 setup(
     name='HiveMind-matrix-bridge',
-    version='0.0.2',
+    version=get_version(),
     packages=['hm_matrix_bridge'],
     url='https://github.com/JarbasHiveMind/HiveMind-matrix-bridge',
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
-    description='',
-    install_requires=["hivemind_bus_client",
-                      "python-matrixbot"],
+    description='matrix bridge for hivemind',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    install_requires=required("requirements.txt"),
     entry_points={
         'console_scripts': [
             'HiveMind-matrix=hm_matrix_bridge.__main__:main'
